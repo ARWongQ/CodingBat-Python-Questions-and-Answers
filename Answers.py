beebe4995579a5dabacf53ca58c9e223fdc9c1d3
Augusto Rolando Wong <augustowong2010@hotmail.com>
2016-12-10 14:06:19 -0500

    Update Answers.py

--- Answers.py
+++ Answers.py
@@ -34,15 +34,43 @@ def not_string(str):
   else:
     return "not " + str
 
-  #3
-  '''
-  str = string
-  n = chracter to be removed
-  '''
-  def missing_char(str, n):
-    str_length = len(str)
-    removing = str[n]
-    removed= str.replace(removing,"")
-    return removed
-  
-  
+#3
+'''
+str = string
+n = chracter to be removed
+'''
+def missing_char(str, n):
+  str_length = len(str)
+  removing = str[n]
+  removed= str.replace(removing,"")
+  return removed
+
+#4
+def front_back(str):
+  str_length = len(str)
+  if(str_length <= 1):
+    return str
+    
+  else:
+    first_letter= str[0]
+    last_letter = str[str_length - 1]
+    middle = str[1:str_length -1]
+    
+    return last_letter + middle + first_letter
+
+#5
+def front3(str):
+  str_length = len(str)
+  if(str_length < 3):
+    first_letters = str[0:str_length]
+
+    for i in range(0,2):
+      str += first_letters
+    return str
+    else:
+      first_letter= str[0:3]
+      new = ""
+      for i in range(0,3):
+          new += first_letter
+
+      return new
