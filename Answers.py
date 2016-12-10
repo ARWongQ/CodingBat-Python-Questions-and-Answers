5d5ce54f296982553888cd91dbd9eb70b3b40962
Augusto Rolando Wong <augustowong2010@hotmail.com>
2016-12-10 13:34:19 -0500

    Update Answers.py

--- Answers.py
+++ Answers.py
@@ -1,14 +1,20 @@
 #1
 def pos_neg(a, b, negative):
+  #check if negative
   if(negative):
+    #return true if both numbers are negative
     return (a < 0) and (b < 0)
     
   else:
+    #check if a is positive and b is negative
     if( a > 0) and (b < 0):
       return True
-      
+    
+    #check if a is negative and b is positive
     elif( a < 0) and (b > 0):
       return True
-      
+    #return False is neither case was achieved
     else:
       return False
+
+#2
