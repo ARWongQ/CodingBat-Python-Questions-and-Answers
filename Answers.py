352d0efb119ed64323281e7e2846f0cc3420c867
Augusto Rolando Wong <augustowong2010@hotmail.com>
2016-12-17 19:41:11 -0500

    new answers
    total number of answers is 24

--- Answers.py
+++ Answers.py
@@ -224,3 +224,227 @@ def array_front9(nums):
   #if not found return false
   return Flag_9
 
+#12
+'''
+@parameters
+nums = array of numbers
+'''
+def array123(nums):
+  #Get the length of the array
+  length = len(nums)
+  #loop length-2 times (to not get off the array)
+  for i in range(length-2):
+    #check if the sequence is in the array
+    if(nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3):
+      return True
+
+  return False
+
+#13
+'''
+@parameters
+a = string
+b = string
+'''
+def string_match(a, b):
+  #Get the length of the two given strings
+  length_a = len(a)
+  length_b = len(b)
+  #Get the smallest string
+  min_length = min(length_b, length_a)
+  #Keep track of the # of times they contain the same length 2 substring
+  count = 0
+  #Check if length is less than 2
+  if(min_length < 2):
+    return count
+
+  else:
+    #loop length-1 (to not go off the array)
+    for i in range(min_length-1):
+      #check if the length 2 substring are the same (if so add 1 to count)
+      if(a[i] == b[i] and a[i+1] == b[i+1]):
+        count += 1
+
+  return count
+
+#14
+'''
+@parameters
+name = string
+'''
+def hello_name(name):
+  #create greeting
+  greeting= "Hello " + name + "!"
+  return greeting
+
+#15
+'''
+@parameters
+a = string
+b = string
+'''
+def make_abba(a, b):
+  #concatenate the words in the proper abba order
+  combined= a + b + b + a
+  return combined
+
+#16
+'''
+@parameters
+tag = string
+word = string
+'''
+def make_tags(tag, word):
+  #Create the beginning tag
+  HTML_tag_1= "<" + tag + ">"
+  #Create the end tag
+  HTML_tag_2= "</" + tag + ">"
+  
+  #Concatenate in proper order
+  HTML_string = HTML_tag_1 + word + HTML_tag_2
+
+  return HTML_string
+
+#17
+'''
+@parameters
+out = string
+word = string
+'''
+def make_out_word(out, word):
+  #Break the out word in half
+  out_b = out[0:2]
+  out_e = out[2:4]
+  
+  #Concatenate in proper order (sandwich)
+  combined= out_b + word + out_e
+
+  return combined
+
+#18
+'''
+@parameters
+str = string
+'''
+def extra_end(str):
+  #Get the proper lengths 
+  length = len(str)
+  prev_len = length - 2
+  #Get the last 2 characters
+  last_c = str[prev_len:length]
+  #Variable to hold the answer
+  new = ""
+  #loop 3 times to concatenate the last characters
+  for i in range(3):
+    new += last_c
+
+  return new
+
+#19
+'''
+@parameters
+str = string
+'''
+def first_two(str):
+  #Get the length of strings
+  length = len(str)
+  end= 2
+  #Check if the length is smaller than two (to not go off the array 
+  if(length < 2):
+    n = length
+  #get the first end characters of the string
+  first = str[0:end]
+
+  return first
+  
+    
+#20
+'''
+@parameters
+str = string
+'''
+def first_half(str):
+  #Get the length of the string
+  length = len(str)
+  #Return the first half of the given string
+  return str[0:length/2]
+
+#21
+'''
+@parameters
+str = string
+'''
+def without_end(str):
+  #get the length of the string
+  length = len(str)
+  #variable to hold the answer
+  new= ""
+  #check if length is smaller than 2 
+  if(length <= 2):
+    return new
+  
+  else:
+    #save the middle characters of the given string
+    middle = str[1:length-1]
+
+  return middle
+    
+#22
+'''
+@parameters
+a = string
+b = string
+'''
+def combo_string(a, b):
+  #Get the lengths of the two given strings
+  length_a = len(a)
+  length_b = len(b)
+  #if a is bigger do bab
+  if(length_a >= length_b):
+    return b + a + b
+  else:
+    #if b is bigger do aba
+    return a + b + a
+
+#23
+'''
+@parameters
+a = string
+b = string
+'''
+def non_start(a, b):
+  #Get the lengths of the two hiven strings
+  length_a = len(a)
+  length_b = len(b)
+  #remove the first characters of each string 
+  new_a = a[1:length_a]
+  new_b = b[1:length_b]
+  #concatenate the words
+  combined = new_a + new_b
+
+  return combined
+
+#24
+'''
+@parameters
+str = string
+'''
+def left2(str):
+  #get the length of the string
+  length = len(str)
+  #Check if less than or equal to 2
+  if(length <= 2):
+    return str
+
+  else:
+    #Get the first two characters 
+    beg = str[0:2]
+    #Get the remaining of the string
+    rem = str[2:length]
+    
+    #Concatenate remaining and beginning
+    combined = rem + beg
+
+    return combined
+    
+
